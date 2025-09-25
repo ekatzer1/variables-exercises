836232e9a81606988d10e0c9aa9dd11f594fb0d8
Mr. K <ekatzer@sjcawi.org>
2025-09-25 08:19:09 -0500

    first commit

---
+++ circle.js
@@ -0,0 +1,7 @@
+var pi = Math.PI;
+var r = 5;
+
+var A = pi * r**2;
+var C = 2 * pi * r;
+
+console.log("With a circle of radius " + r + ", it has an area of " + A + " and a circumference of " + C + ".");
---
+++ sphere.js
@@ -0,0 +1,7 @@
+var pi = Math.PI;
+var r = 5;
+
+var A = 4 * pi * r**2;
+var V = 4 / 3 * pi * r**3;
+
+console.log("With a sphere of radius " + r + ", it has an surface area of " + A + " and a volume of " + V + ".");
